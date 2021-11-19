581a324009210d3fb7bd271745cf0cf6ac2aa59b
Jennifer-Blue-Williams <jennifer.blue.williams@gmail.com>
2021-11-19 11:22:03 -0600

    for loop works

--- leonids-toys.js
+++ leonids-toys.js
@@ -24,11 +24,37 @@ const toys = [
     inStock: true
 }
 ]
-console.log(toys)
+
+const armyguy = {
+    id: 4,
+    name: "G.I. Joe",
+    manufacturer: "Hasbro",
+    size: "small",
+    price: 1.99,
+    inStock: true
+}
+    
+const capgun = {
+    id: 5,
+    name: "Cap Gun",
+    manufacturer: "Nichols",
+    size: "small",
+    price: 7.49,
+    inStock: false
+}
+
+toys.push(armyguy)
+// console.log(toys)
+toys.push(capgun)
+// console.log(toys)
 
 for (const toy of toys) {
-    console.log(toy)
+    console.log(toy.size)
 }
-for (const toy of toys) {
-    console.log(toy.price)
-}
+
+
+
+
+
+
+
